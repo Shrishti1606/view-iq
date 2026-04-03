05390285c2538ce29848e7b0b833c96e72ca072f
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-04-03 17:15:17 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+import { ENV } from "./env.js";
+
+export const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(ENV.DB_URL)
+        console.log("connected to mongoose:",conn.connection.host)
+    } catch(error){
+        console.log("error connectiong to mongoose", error)
+        process.exit(1); //0->success, 1->failure 
+    }
+};
